1c42c2c5522eb0079590f2443886a36bd3a6e483
Brett Wallace <bwallace90@gmail.com>
2015-03-12 12:56:10 -0400

    Learn dictionaries and implement TaskModel

---
+++ TaskIt/TaskModel.swift
@@ -0,0 +1,15 @@
+//
+//  TaskModel.swift
+//  TaskIt
+//
+//  Created by Brett Wallace on 3/12/15.
+//  Copyright (c) 2015 Brett Wallace. All rights reserved.
+//
+
+import Foundation
+
+struct TaskModel {
+    var task: String
+    var subtask: String
+    var date: String
+}
--- TaskIt/ViewController.swift
+++ TaskIt/ViewController.swift
@@ -11,9 +11,16 @@ import UIKit
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     @IBOutlet weak var tableView: UITableView!
 
+    var taskArray: [TaskModel] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let task1 = TaskModel(task: "Study French", subtask: "Verbs", date: "01/14/2014")
+        let task2 = TaskModel(task: "Eat dinner", subtask: "Burgers", date: "01/14/2014")
+        
+        taskArray = [task1, task2, TaskModel(task: "Gym", subtask: "Leg day", date: "01/14/2014")]
     }
 
     override func didReceiveMemoryWarning() {
@@ -23,15 +30,16 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
 
     // UITableViewDataSource
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return taskArray.count
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let thisTask = taskArray[indexPath.row]
         var cell: TaskCell = tableView.dequeueReusableCellWithIdentifier("myCell") as TaskCell
         
-        cell.taskLabel.text = "Study French"
-        cell.descriptionLabel.text = "Verbs in past and present"
-        cell.dateLabel.text = "01/10/2014"
+        cell.taskLabel.text = thisTask.task
+        cell.descriptionLabel.text = thisTask.subtask
+        cell.dateLabel.text = thisTask.date
         
         return cell
     }

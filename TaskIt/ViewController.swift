a47dd8d1c538937473cbbb37c6ed1f7c3fe4b7e1
Brett Wallace <bwallace90@gmail.com>
2015-03-11 22:47:10 -0400

    Add labels to TaskCell

--- TaskIt/ViewController.swift
+++ TaskIt/ViewController.swift
@@ -23,11 +23,17 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
 
     // UITableViewDataSource
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return 5
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        return UITableViewCell()
+        var cell: TaskCell = tableView.dequeueReusableCellWithIdentifier("myCell") as TaskCell
+        
+        cell.taskLabel.text = "Study French"
+        cell.descriptionLabel.text = "Verbs in past and present"
+        cell.dateLabel.text = "01/10/2014"
+        
+        return cell
     }
     
     // UITableViewDelegate

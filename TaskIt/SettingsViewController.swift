0cc9cf3b1a8f8467dc2bcac79f74e8570ede06bd
Brett Wallace <bwallace90@gmail.com>
2015-06-23 16:37:13 -0400

    Set up viewDidLoad for SettingsViewController

--- TaskIt/SettingsViewController.swift
+++ TaskIt/SettingsViewController.swift
@@ -8,19 +8,40 @@
 
 import UIKit
 
-class SettingsViewController: UIViewController {
+class SettingsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     @IBOutlet weak var capitalizeTableView: UITableView!
     @IBOutlet weak var completeNewTodoTableView: UITableView!
     @IBOutlet weak var versionLabel: UILabel!
+    
+    let kVersionNumber = "1.0"
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        self.capitalizeTableView.delegate = self
+        self.capitalizeTableView.dataSource = self
+        self.capitalizeTableView.scrollEnabled = false
+        
+        self.completeNewTodoTableView.delegate = self
+        self.completeNewTodoTableView.dataSource = self
+        self.completeNewTodoTableView.scrollEnabled = false
+        
+        self.title = "Settings"
+        
+        self.versionLabel.text = kVersionNumber
+        
+        var doneButton = UIBarButtonItem(title: "Done", style: UIBarButtonItemStyle.Plain, target: self, action: Selector("doneBarButtonItemPressed:"))
+        self.navigationItem.leftBarButtonItem = doneButton
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func doneBarButtonItemPressed(barButtonItem: UIBarButtonItem) {
+        self.navigationController?.popViewControllerAnimated(true)
     }
 }

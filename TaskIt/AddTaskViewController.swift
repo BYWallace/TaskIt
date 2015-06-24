031dad958254f469a63fbfb05d17f545427c1009
Brett Wallace <bwallace90@gmail.com>
2015-06-23 20:05:34 -0400

    Implement capitalization and complete on create settings when adding new task

--- TaskIt/AddTaskViewController.swift
+++ TaskIt/AddTaskViewController.swift
@@ -47,10 +47,23 @@ class AddTaskViewController: UIViewController {
         let entityDescription = NSEntityDescription.entityForName("TaskModel", inManagedObjectContext: managedObjectContext!)
         let task = TaskModel(entity: entityDescription!, insertIntoManagedObjectContext: managedObjectContext!)
         
-        task.task = taskTextField.text
+        if NSUserDefaults.standardUserDefaults().boolForKey(kShouldCapitalizeTaskKey) == true {
+            task.task = taskTextField.text.capitalizedString
+        }
+        else {
+            task.task = taskTextField.text
+        }
+    
         task.subtask = subtaskTextField.text
         task.date = dueDatePicker.date
-        task.completed = false
+        
+        if NSUserDefaults.standardUserDefaults().boolForKey(kShouldCompleteNewTodoKey) == true {
+            task.completed = true
+        }
+        else {
+            task.completed = false
+        }
+        
         
         appDelegate.saveContext()
         
